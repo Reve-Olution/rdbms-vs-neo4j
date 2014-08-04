c9c8aa437cf47786a0dc2bd2ee51a9e839165dfa
arnaudgeiser <arnaudgeiser@gmail.com>
2014-08-04 18:14:50 +0200

    Supppression de la base Neo4J avant création

--- src/groovy/ch/waterbead/neo4jloader/GlobalNeo4Loader.groovy
+++ src/groovy/ch/waterbead/neo4jloader/GlobalNeo4Loader.groovy
@@ -1,7 +1,12 @@
 package ch.waterbead.neo4jloader
 
+import ch.waterbead.config.Config
+
 class GlobalNeo4Loader {
     def static load(def abilities, def projects, def groups, def employes) {
+        File file = new File(Config.neo4JPath);
+        file.deleteDir();
+        
         AbilityNeo4JLoader aloader = new AbilityNeo4JLoader()
         ProjectNeo4JLoader ploader = new ProjectNeo4JLoader()
         GroupNeo4JLoader gloader = new GroupNeo4JLoader()
